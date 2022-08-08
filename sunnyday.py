70823dd284c207b5e6901e70be20a21562753fe0
Van Giang <vangiang260694@gmail.com>
2022-08-08 15:21:53 +0700

    Raising Errors When Arguments Are Not Passed

--- sunnyday.py
+++ sunnyday.py
@@ -1,19 +1,25 @@
 import requests
 
-url = "https://api.openweathermap.org/data/2.5/forecast?q=Ho%20Chi%20Minh&appid=be63187149284bb1bae3e00e08f31774&units=metric"
-r = requests.get(url)
-print(r)
-print(r.json())
-
 
 class Weather:
 
-	def __init__(self, apikey, city, lat, lon):
-		url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apikey}&units=metric"
-		r = requests.get(url)
+	def __init__(self, apikey, city, lat=None, lon=None):
+		if city:
+			url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apikey}&units=metric"
+			r = requests.get(url)
+			self.data = r.json()
+		elif lat and lon:
+			url = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apikey}&units=metric"
+			r = requests.get(url)
+			self.data = r.json()
+		else:
+			raise TypeError("Provide Either A City Or Lat And Lon Arguments")
 
 	def next_12h(self):
 		pass
 
 	def next_12h_simplified(self):
 		pass
+
+weather = Weather(apikey="be63187149284bb1bae3e00e08f31774", city="Di An")
+print(weather.data)
